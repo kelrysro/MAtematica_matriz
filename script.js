81d6e77a824fad7a55d80cf7021881794ee60b6e
kelrysro <kelryrodrigues86@gmail.com>
2023-06-23 09:17:20 -0300

    teste

---
+++ script.js
@@ -0,0 +1,8 @@
+let tr = Number(prompt("Quantas colunas?"))
+let td = Number(prompt("Quantas linhas?"))
+
+const totalelementos = tr * td
+
+for (let linha = 0;  linha < td.length; ++) {
+    const element = Number(prompt(`linha ${td} inscira o valor dos numeros da coluna ${td}`))    
+}
